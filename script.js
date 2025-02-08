0261a9cf407b8c52636d4420c8cf5392d33f298d
Jason Merino-Garcia <138510804+JasonMerino-4@users.noreply.github.com>
2025-02-08 10:54:53 -0600

    Add files via upload
    skeleton

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const category = document.getElementById("budget");
+    const craving  = document.getElementById("craving");
+    const distance = document.getElementById("distance_radius");
+    const eat      = document.getElementById("eat_button");
+
+    eat.addEventListener("click", function(){
+        //do something
+    })
+});
